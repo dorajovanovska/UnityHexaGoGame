e1f9c2f7d2a80789985cd3747ae789b9b43bedb8
dorajovanovska <dora.jovanovska@gmail.com>
2021-01-13 15:03:13 +0100

    Ninth commit
    When Player collects all of the Goal Hexacoins, info Canvas is shown on-screen. Mentioned Canvas informs the Player that he/she needs to find a Portal before Level countdown runs out of time. Additional modification needed.

---
+++ HexaGo/Assets/Scripts/GoToTheNextLevel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToTheNextLevel : MonoBehaviour
+{
+    public PlayerController playerController;
+    public Animator transition_goal;
+    public Animator transition_canvas;
+    public Animator transition_score;
+    public Animator transition_lives;
+    public Animator transition_levelup;
+    public Animator transition_countdown;
+    public float end_duration = 0.1f;
+
+    void Update()
+    {
+        if (playerController.hexaGoalTrue == true)
+        {
+            transition_goal.SetTrigger("GoalAchievedFadeOutTrigger");
+        }
+            
+    }
+
+    IEnumerator GoToNextLevel()
+    {
+        transition_score.SetTrigger("ScoreTextFadeOut");
+        transition_lives.SetTrigger("LivesTextFadeOut");
+        transition_levelup.SetTrigger("LevelUpFadeOutTrigger");
+        transition_canvas.SetTrigger("FadeOutTrigger");
+        transition_countdown.SetTrigger("CountFadeOut");
+        yield return new WaitForSeconds(end_duration);
+        SceneManager.LoadScene("Level_02");
+    }
+}
