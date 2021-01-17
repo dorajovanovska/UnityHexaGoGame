c8d323e1953687da7f96eda2da315edebb9281f7
dorajovanovska <dora.jovanovska@gmail.com>
2021-01-17 01:17:37 +0100

    Twelfth commit
    Main menu with Start Game option created (design and partial functionality). Main menu scene is placed after the Intro video scene and before the first level. When Player clicks on the Start Game option, a new game starts at first level. Further modifications that are needed, refer to functionality of the remaining Main menu options (e.g. How to play, Quit etc.).

--- HexaGo/Assets/Scripts/GameManager.cs
+++ HexaGo/Assets/Scripts/GameManager.cs
@@ -39,7 +39,7 @@ public class GameManager : MonoBehaviour
 
     public void RestartLevel()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene("MainMenu");
     }
 
 }
