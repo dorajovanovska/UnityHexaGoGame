6cc1b51466a20800e907bdc7629dba1e87d24c87
dorajovanovska <dora.jovanovska@gmail.com>
2021-01-18 21:44:39 +0100

    15th commit
    Custom sound effects for mouse hover and click on the Main menu buttons. Design layout for How To Play option.

---
+++ HexaGo/Assets/Scripts/MainMenuScene/PlaySoundOnButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnButton : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip hoverAudioClip;
+    public AudioClip clickAudioClip;
+
+    public void PlayHoverSound()
+    {
+        audioSource.PlayOneShot(hoverAudioClip);
+    }
+
+    public void PlayClickSound()
+    {
+        audioSource.PlayOneShot(clickAudioClip);
+    }
+}
